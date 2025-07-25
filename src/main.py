33ce2130324adeafe0224de1407eda85d50748d9
TheStreetsAreCalling <kazubike0971@gmail.com>
2025-07-25 13:01:20 -0400

    testing on other computers

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 import numpy as np
 from ultralytics import YOLO
 import cv2
+import os
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+contents = os.listdir(script_directory)
+video_path = ""
+for f in os.listdir(script_directory):
+    if '.mp4' in f:
+        video_path = repr(f)
+    print(repr(f))
+
+# print("Script's Directory:", script_directory)
+
+print(os.path.exists(video_path))
 
 model = YOLO("yolov8n.pt")
  
-results = model("../traindata/video1.mp4", show=True)
+results = model(video_path, show=True)
 print("all done")
