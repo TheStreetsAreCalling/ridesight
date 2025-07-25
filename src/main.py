1588a1be3c4c0cc7b8cde006968847870d88f75f
TheStreetsAreCalling <kazubike0971@gmail.com>
2025-07-25 13:46:28 -0400

    working test

--- src/main.py
+++ src/main.py
@@ -3,19 +3,22 @@ from ultralytics import YOLO
 import cv2
 import os
 
+
+vision_models = ["YOLO", "OPENCV", "DLIB"]
+
 script_directory = os.path.dirname(os.path.abspath(__file__))
 contents = os.listdir(script_directory)
 video_path = ""
 for f in os.listdir(script_directory):
     if '.mp4' in f:
-        video_path = repr(f)
+        video_path = f
     print(repr(f))
 
-# print("Script's Directory:", script_directory)
-
-print(os.path.exists(video_path))
-
+print("Exists?", os.path.exists(os.path.join(script_directory, video_path)))
+print("Using " + vision_models[0] + " on video: " + video_path)
+      
 model = YOLO("yolov8n.pt")
  
 results = model(video_path, show=True)
+results.save()
 print("all done")
